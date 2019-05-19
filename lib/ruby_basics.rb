0483ce54f3cbfa6420b97723ccaf8dcef9fefbba
Ryan Bravo <rrbravo2@gmail.com>
2019-05-19 04:29:29 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,21 +1,24 @@
-def division(num1, num2)
- 
+def division(num1=42, num2=7)
+ puts num1 / num2
+ return 6
 end
 
 def assign_variable(value)
+  "#{value}" 
+end
+
+def argue(phrase)
+  "#{phrase}"
   
 end
 
-def argue
-  
-end
-
-def greeting
+def greeting(greetin, name)
+  greeting, name 
 
 end
 
 def return_a_value
-
+ "Nice"
 end
 
 def last_evaluated_value
